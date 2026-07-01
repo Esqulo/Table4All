8cdcac9f3c757e97eb792ebbb13c36788343054a
Juan Elias da Cunha <92590764+Esqulo@users.noreply.github.com>
2026-07-01 19:16:08 -0300

    fix linter

--- app/Models/QueueItem.php
+++ app/Models/QueueItem.php
@@ -6,6 +6,9 @@ use App\Enums\QueueItemStatus;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property QueueItemStatus $status
+ */
 final class QueueItem extends Model
 {
     protected $fillable = [
@@ -23,16 +26,19 @@ final class QueueItem extends Model
         ];
     }
 
+    /** @return BelongsTo<RestaurantTable, $this> */
     public function restaurantTable(): BelongsTo
     {
         return $this->belongsTo(RestaurantTable::class);
     }
 
+    /** @return BelongsTo<Product, $this> */
     public function product(): BelongsTo
     {
         return $this->belongsTo(Product::class);
     }
 
+    /** @return BelongsTo<RestaurantQueue, $this> */
     public function queue(): BelongsTo
     {
         return $this->belongsTo(RestaurantQueue::class, 'queue_id');
